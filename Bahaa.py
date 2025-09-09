221dbdeb0b15309aeb2a1e0578956f919725381d
bahavelikolep-netizen <bahavelikolep@gmail.com>
2025-09-09 14:17:14 +0500

    Add files via upload

---
+++ Bahaa.py
@@ -0,0 +1,11 @@
+a = 12
+b = 5
+while True(6):
+    print("a + b=", a + b)
+    print("a - b=", a - b)
+    print("a * b=", a * b)
+    print("a ** b=", a ** b)
+    print("a / b=", a / b)
+    print("a // b=", a // b)
+    print("a % b=", a % b)
+    break
