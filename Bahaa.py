09b4c26816a74ac902e12e6a15819dd5ffb366c5
bahavelikolep-netizen <bahavelikolep@gmail.com>
2025-09-09 14:40:04 +0500

    Add files via upload

--- Bahaa.py
+++ Bahaa.py
@@ -1,11 +1,18 @@
-a = 12
-b = 5
-while True(6):
-    print("a + b=", a + b)
-    print("a - b=", a - b)
-    print("a * b=", a * b)
-    print("a ** b=", a ** b)
-    print("a / b=", a / b)
-    print("a // b=", a // b)
-    print("a % b=", a % b)
-    break
+#Ist = [1,2,3,4,5]
+#print("Исходный список:", Ist)
+#print("Обратный  список:", Ist[::-1])
+
+#def list_sort(Ist):
+#    return sorted(Ist, key=abs, reverse=True)
+#numbers = [5, -4, 7, -14, 8]
+#print("Исходный список:", numbers)
+#print("Сортировка по Убыванию |x|:", list_sort(numbers))
+
+
+def change(Ist):
+    if len(Ist) >=2:
+        Ist[0], Ist[-1] = Ist[-1], Ist[0]
+    return Ist
+Data = [20,30,40,50]
+print("До изменения:", Data)
+print("После изменения:", change(Data))
--- Sultan Karali.py
+++ Sultan Karali.py
@@ -1,10 +1,17 @@
-a = 12
-b = 5
+#Ist = [1,2,3,4,5]
+#print("Исходный список:", Ist)
+#print("Обратный  список:", Ist[::-1])
 
-print("a + b=", a + b)
-print("a - b=", a - b)
-print("a * b=", a * b)
-print("a ** b=", a ** b)
-print("a / b=", a / b)
-print("a // b=", a // b)
-print("a % b=", a % b)
+#def list_sort(Ist):
+#    return sorted(Ist, key=abs, reverse=True)
+#numbers = [3, -7, 1, -10, 4]
+#print("Исходный список:", numbers)
+#print("Сортировка по Убыванию |x|:", list_sort(numbers))
+
+def change(Ist):
+    if len(Ist) >=2:
+        Ist[0], Ist[-1] = Ist[-1], Ist[0]
+    return Ist
+Data = [10,20,30,40]
+print("До изменения:", Data)
+print("После изменения:", change(Data))
